b37402db5299f0fa1da5e7e5ed7271330acabbba
고범석 <kobums23@gmail.com>
2023-06-08 00:00:05 +0900

    init

---
+++ src/main/java/com/example/controlhabitspring/ControlhabitspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.controlhabitspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlhabitspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlhabitspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/controlhabitspring/ControlhabitspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.controlhabitspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlhabitspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
